28cb3aee758f0c10fccf84053e4eb74cfc199cab
wasabi <richardlikeswasabi@gmail.com>
2017-09-11 18:07:54 +1000

    Completed MVP objectives

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 ## Important
 + pip3 install newspaper3k
 + pip3 install python_wtf
++ Make sure you have nltk installed and downloaded. (nltk.download() and find punkt)
 
 ## Usage
 
--- app/views.py
+++ app/views.py
@@ -3,28 +3,37 @@ from flask import render_template
 from app import app
 
 # Other things
-from flask import flash,request
-from .forms import ArticleForm
+from flask import request
 from newspaper import Article
+import nltk
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    article_text = "no article loaded"
+    article_title = ""
+    article_summary = ""
+    article_original = ""
 
-    form = ArticleForm()
-    #print(form.article.data)
-    #print(request.args.get('url', None))
-    if form.validate_on_submit():
-        url = form.article.data
+    #FIXME sanitize url
+    url = request.args.get('url', None)
+    if url != None:
         article = Article(url)
         article.download()
         article.parse()
+        article_title = article.title
         article_text = article.text
+        # Comment out if you have not installed nlpt
+        article.nlp()
+        article_summary = article.summary
+
+
+
+
 
     return render_template('index.html',
                             title='Summariser',
-                            article=article_text,
-                            form=form)
+                            article_title=article_title,
+                            article_summary=article_summary,
+                            article_original=article_text)
 
     

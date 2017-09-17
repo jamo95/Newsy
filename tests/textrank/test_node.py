a51b298c3d3b97e1886290bd4f6d6de4f13c7055
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-09-17 15:51:09 +1000

    Setup Pytest and add tests for textrank graph node

---
+++ tests/textrank/test_node.py
@@ -0,0 +1,45 @@
+from app.textrank.node import Node
+
+
+def test_add_variation():
+    node = Node('a')
+    node.add_variation('v')
+
+    assert set(node._variations) == set(['v'])
+
+
+def test_remove_variation():
+    node = Node('a')
+    node._variations = ['v1', 'v2']
+    node.remove_variation('v1')
+
+    assert set(node._variations) == set(['v2'])
+
+
+def test_has_variation():
+    node = Node('a')
+    node._variations = ['v']
+
+    assert node.has_variation('v')
+    assert not node.has_variation('w')
+
+
+def test_get_variations():
+    node = Node('a')
+    node._variations = ['v']
+
+    assert set(node.get_variations()) == set(['v'])
+
+
+def test_get_averaged_score_variations():
+    node = Node('a', score=10)
+    node._variations = ['v1', 'v2']
+
+    assert node.get_averaged_score() == 5.0
+
+
+def test_get_averaged_score_data():
+    node = Node('a', score=10)
+
+    assert len(node._variations) == 0
+    assert node.get_averaged_score() == 10.0

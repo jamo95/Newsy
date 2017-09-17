e8486d93950801827791f836cd795cb19ca06508
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-09-17 17:20:21 +1000

    Implement textrank graph

---
+++ app/textrank/graph.py
@@ -0,0 +1,61 @@
+from app.textrank.node import Node
+
+
+# This is an undirected, unweighted graph represented by an adjacnecy list.
+class Graph:
+    def __init__(self):
+        # Adjacency list of node to list of nodes. The key node represents the
+        # initial node in the edge, and each node in the list represents an
+        # edge where it is the final node.
+        self._adjacency_list = {}
+
+    def add_node(self, node):
+        '''Add node to the graph.'''
+
+        if node not in self._adjacency_list:
+            self._adjacency_list[node] = []
+
+    def add_edge(self, node_i, node_f):
+        '''Add edge to the graph. If node_i or node_f doesn't exist then it
+        will add them to the graph.'''
+
+        self.add_node(node_i)
+        self.add_node(node_f)
+
+        if node_f not in self._adjacency_list[node_i]:
+            self._adjacency_list[node_i].append(node_f)
+
+    def get_nodes_count(self):
+        '''Return the number of nodes in the graph.'''
+
+        return len(self._adjacency_list.keys())
+
+    def get_edges_count(self):
+        '''Return the number of edges in the graph.'''
+
+        return sum(map(len, self._adjacency_list.values()))
+
+    def has_node(self, data):
+        '''Return true if there exists a node with the specified data.'''
+
+        return Node(data) in self._adjacency_list
+
+    def get_nodes(self):
+        '''Get all nodes in the graph.'''
+
+        return self._adjacency_list.keys()
+
+    def get_connected_to(self, node_f):
+        '''Get all the nodes connected to final node `node_f`.'''
+
+        connected_to = []
+        for i, f in self._adjacency_list.items():
+            if node_f in f:
+                connected_to.append(i)
+
+        return connected_to
+
+    def get_connected_from(self, node_i):
+        '''Get all the nodes connected to initial node `node_i`.'''
+
+        return self._adjacency_list[node_i]

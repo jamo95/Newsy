ed1559f4fb7a3bfcb78c3b628c4bf8ea3582b4b1
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-09-29 15:56:38 +1000

    Add tests for running textrank on sentences

---
+++ tests/textrank/test_sentences.py
@@ -0,0 +1,20 @@
+from app.textrank import sentences
+from app.textrank.node import Node
+
+
+def test_rank():
+    sentence_list = [
+        'This is a sentence.',
+        'A quick brown fox.',
+        'Jumped over the lazy dog.'
+    ]
+
+    sentence_nodes = [
+        Node(s, score=sentences.DEFAULT_NODE_SCORE) for s in sentence_list]
+
+    # Here we expect that each sentence node will have the same score. This is
+    # because the 'clean' (non-stop) words in each sentence have no similarity
+    # with any other sentence.
+    for i in range(len(sentence_list)):
+        assert sentence_nodes[i].data == sentence_list[i]
+        assert sentence_nodes[i].score == sentences.DEFAULT_NODE_SCORE

83410072cccbe7696cc64e79ee526cd3dfdbe973
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-09-29 16:15:07 +1000

    Fix textrank sentence ranking tests

--- tests/textrank/test_sentences.py
+++ tests/textrank/test_sentences.py
@@ -3,14 +3,13 @@ from app.textrank.node import Node
 
 
 def test_rank():
-    sentence_list = [
-        'This is a sentence.',
-        'A quick brown fox.',
-        'Jumped over the lazy dog.'
-    ]
+    sentence_list = sorted([
+        'This is a sentence.', 'A quick brown fox.', 'Jumped over the lazy dog.'
+    ])
 
-    sentence_nodes = [
-        Node(s, score=sentences.DEFAULT_NODE_SCORE) for s in sentence_list]
+    sentence_nodes = sentences.rank([
+        Node(s, score=sentences.DEFAULT_NODE_SCORE) for s in sentence_list])
+    sentence_nodes = sorted(sentence_nodes, key=lambda n: n.data)
 
     # Here we expect that each sentence node will have the same score. This is
     # because the 'clean' (non-stop) words in each sentence have no similarity

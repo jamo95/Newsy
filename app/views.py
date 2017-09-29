bc1fded0bff04540a4163700028cf0e24972a076
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-09-29 16:39:08 +1000

    Use our textrank algorithm with a site

--- app/views.py
+++ app/views.py
@@ -1,45 +1,46 @@
-from flask import render_template
+from newspaper import Article
+from flask import render_template, request
 
 from app import app
+from app.textrank.sentences import rank as rank_sentences
+from app.textrank.node import Node
+from app.textrank.helpers import tokenize_sentences
 
-# Other things
-from flask import request
-from newspaper import Article
-import nltk
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/summarised', methods=['GET', 'POST'])
 def summarised():
-    article_title = ""
-    article_keywords = ""
-    article_summary = ""
-    article_original = ""
+    ctx = {
+        'title': 'Summariser', 'keywords': '', 'sentences': ''
+    }
 
-    #FIXME sanitize url
+    # TODO: Sanitize URL.
     url = request.args.get('url', None)
-    if url != None:
-        article = Article(url)
-        article.download()
-        article.parse()
-        article_title = article.title
-        article_original = article.text
-        # Comment out if you have not installed nlpt
-        article.nlp()
-        article_keywords = article.keywords
-        article_summary = article.summary
+
+    if url is not None:
+        article = _get_article_from_url(url)
+        ctx['title'] = article.title
+
+        sentences = tokenize_sentences(article.text)
+        sentence_nodes = sorted(
+            rank_sentences([Node(s) for s in sentences]),
+            key=lambda n: n.score, reverse=True
+        )
+
+        sorted_sentences = [node.data for node in sentence_nodes]
+        ctx['sentences'] = sorted_sentences[:10]
+
+    return render_template('summarised.html', **ctx)
 
 
+def _get_article_from_url(url):
+    article = Article(url)
 
+    article.download()
+    article.parse()
 
-
-    return render_template('summarised.html',
-                            title='Summariser',
-                            article_title=article_title,
-                            article_keywords=article_keywords,
-                            article_summary=article_summary,
-                            article_original=article_original)
-
-    
+    return article

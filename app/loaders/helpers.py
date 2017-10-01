0cc7916c4f7db637b9b2013f88f8bacad2f7e880
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-10-01 18:15:21 +1100

    Fix up techcrunch loader

---
+++ app/loaders/helpers.py
@@ -0,0 +1,10 @@
+import re
+
+
+def clean_html(html):
+    # Remove header tags including content.
+    html = re.sub(r'<h(\d).*?</h\1>', '', html)
+    # Strip surrounding tags.
+    text = re.sub(r'<.*?>', '', html)
+    # Strip surrounding whitespace.
+    return text.strip()
--- app/loaders/testload.py
+++ app/loaders/testload.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python3
 import techcrunch
 
-print("testing techcrunch")
 
-tc = techcrunch.ArchiveLoader.load(2017, 9, 25)
+article = techcrunch.ArticleLoader.load(
+    'https://techcrunch.com/2017/09/30/where-human-intelligence-outperforms-ai')
 
-print("site name is " + tc.site)
-for article in tc.articles:
-    print("title is :" + str(article.title))
-    print("timestamp is :" + str(article.timestamp))
-    print("tags are is :" + str(article.tags))
-    print("links are :" + str(article.links))
-    print("content is :" + str(article.text))
-print("timestamp is " + str(tc.timestamp))
-print("url name is " + str(tc.url))
+
+print(article['content'])

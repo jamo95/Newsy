e5bd06bb04feb13d03a189599bcad0f954461881
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-10-07 17:03:16 +1100

    Add published_at column to article in db

--- app/dao/article.py
+++ app/dao/article.py
@@ -18,22 +18,24 @@ class Article(Base):
     text = Column(Text)
     title = Column(String(200), nullable=True)
     url = Column(String(2083), nullable=True)
+    published_at = Column(Date, nullable=True)
     created_at = Column(Date, default=datetime.datetime.now)
 
     keywords = relationship('Keyword')
     sentences = relationship('Sentence')
 
-    def __init__(self, article_hash, text, title, url):
+    def __init__(self, article_hash, text, title, url, published_at=None):
         self.hash = article_hash
         self.text = text
         self.title = title
         self.url = url
+        self.published_at = published_at
 
         self.keywords = []
         self.sentences = []
 
 
-def insert(session, text, title, url, keywords, sentences):
+def insert(session, text, title, url, keywords, sentences, published_at=None):
     article_hash = hash_article(text)
     article_exists = session.query(Article.hash).filter_by(
         hash=article_hash).scalar() is not None
@@ -41,7 +43,7 @@ def insert(session, text, title, url, keywords, sentences):
     if article_exists:
         return
 
-    article = Article(article_hash, text, title, url)
+    article = Article(article_hash, text, title, url, published_at)
     article.sentences = [
         Sentence(s.data, s.score, s.index) for s in sentences]
 

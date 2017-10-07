d2c70f9ceff724d2883d6e0e8b228512043c7c23
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2017-10-07 12:35:53 +1100

    Add textrank helpers function to normalize urls

--- app/textrank/helpers.py
+++ app/textrank/helpers.py
@@ -20,6 +20,15 @@ def tokenize_words(text, clean=True):
     return tokens
 
 
+def normalize_url(url):
+    clean_url = url.rstrip('/')
+    clean_url = clean_url.replace('http://', '', 1)
+    clean_url = clean_url.replace('https://', '', 1)
+    clean_url = clean_url.replace('www.', '', 1)
+
+    return clean_url
+
+
 def pos_tag_text(text, clean=True):
     '''POS tag a list of tokens.'''
 

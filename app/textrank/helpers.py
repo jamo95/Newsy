11f54529696e76a86be7cd1a14119f87700aac2a
wasabi <richardlikeswasabi@gmail.com>
2017-10-21 01:04:06 +1100

    Added lemmatization

--- app/textrank/helpers.py
+++ app/textrank/helpers.py
@@ -16,13 +16,9 @@ def tokenize_words(text, clean=True):
     tokens = [t.lower() for t in tokens]
 
     if clean:
-        unclean = _unclean_words()
-        tokens = [t for t in tokens if t not in unclean]
-
-        # NN => Noun JJ => Adjective NNP => Pronoun
-        tags = ['NN', 'JJ']
-        tokens = pos_tag_tokens(tokens)
-        tokens = [t[0] for t in tokens if t[1] in tags]
+        tokens = _clean_words(tokens)
+        tagged_tokens = pos_tag_tokens(tokens)
+        tokens = lemmatize(tagged_tokens)
 
     return tokens
 
@@ -36,16 +32,46 @@ def normalize_url(url):
     return clean_url
 
 
-def pos_tag_tokens(tokens, clean=True):
-    '''POS tag a list of tokens.'''
+def pos_tag_tokens(tokens,wanted_tags=['J','N'], clean=True):
+    '''POS tag a list of tokens. 
+    Note that wanted_tags is just start of tag wanted
+    https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html 
+    for definitions '''
 
-    return nltk.pos_tag(tokens)
+    tagged = nltk.pos_tag(tokens)
+    return [t for t in tagged if t[1][0] in wanted_tags]
 
 
-def _unclean_words():
-    '''Gets a list of unclean words.'''
+def _clean_words(words):
+    '''Returns a list of clean words.'''
 
     unclean_words = nltk.corpus.stopwords.words('english')
     unclean_words += string.punctuation
 
-    return unclean_words
+    clean_words = [t for t in words if t not in unclean_words]
+
+    return clean_words
+
+def lemmatize(tagged_tokens):
+    lemmatized = []
+    lemmatizer = nltk.wordnet.WordNetLemmatizer()
+    for t in tagged_tokens:
+        lemma = lemmatizer.lemmatize(
+                t[0], get_wordnet_pos(t[1]))
+
+        lemmatized.append(lemma)
+    return lemmatized
+
+# Cannibalized from a stackoverflow post
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return 'a'
+    elif treebank_tag.startswith('V'):
+        return 'v'
+    elif treebank_tag.startswith('N'):
+        return 'n'
+    elif treebank_tag.startswith('R'):
+        return 'r'
+    else:
+        # If we get here gg...
+        return None

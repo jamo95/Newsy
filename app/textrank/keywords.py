d7328e2ab69c7b1cafeec96ac5be3b4874cf5f74
wasabi <richardlikeswasabi@gmail.com>
2017-10-20 22:43:13 +1100

    Optimised calculating the cooccurrence matrix

--- app/textrank/keywords.py
+++ app/textrank/keywords.py
@@ -32,34 +32,49 @@ def rank_words(title, text):
     # Textrank Algorithm
     graph = Graph()
     seen_words = []
-    _connect_nodes(graph, seen_words, title_words, multiplier=TITLE_MULTIPLIER)
-    _connect_nodes(graph, seen_words, text_words)
+    _connect_nodes(graph, title_words, multiplier=TITLE_MULTIPLIER)
+    _connect_nodes(graph, text_words)
 
     for node in graph.get_nodes():
         _score_node(graph, node)
 
-    #no = list(graph.get_nodes())
     return sorted(graph.get_nodes(), key=lambda n: n.score, reverse=True)
 
 
-def _connect_nodes(graph, seen_words , words, multiplier=1.0):
+def _connect_nodes(graph, words, multiplier=1.0):
     """
     :param graph (Graph)
-    :param seen_words, words (list of words)
+    :param words (list of words)
     :param multiplier - To boost scores of title or other special word sets
     Uses coocurrence in a window of WINDOW_SIZE to create nodes
     target_node and context_node and create edges between these nodes
     """
+    to_connect = _get_cooccurrence_matrix(words)
+
+    for t_word in to_connect:
+        t_node = Node(t_word, multiplier=multiplier)
+        for c_word in to_connect[t_word]:
+            c_node = Node(c_word, multiplier=multiplier)
+
+            graph.add_edge(t_node, c_node)
+            graph.add_edge(c_node, t_node)
+
+def _get_cooccurrence_matrix(words):
+    """
+    :param words (list of words)
+    RETURN: a coocurrence matrix with each row representing target: context_words
+    """
+    to_connect = {}
 
     # Exit if less words than required
     if (len(words) < WINDOW_SIZE):
-        return 
+        return to_connect
 
-    data_index = 0
     # So we can do some fancy optimisations later
     buffer = collections.deque(maxlen=WINDOW_SIZE)
 
     # First window
+    data_index = 0
     for i in range(WINDOW_SIZE):
         buffer.append(words[i])
         data_index = (data_index + 1) % len(words)
@@ -72,23 +87,17 @@ def _connect_nodes(graph, seen_words , words, multiplier=1.0):
             if j == target_index:
                 continue
 
-            if target_word not in seen_words:
-                seen_words.append(target_word)
-                target_node = Node(target_word)
-                target_node.multiplier = multiplier
+            if target_word not in to_connect:
+                to_connect[target_word] = [] 
 
             context_word = buffer[j]
-            if context_word not in seen_words:
-                seen_words.append(context_word)
-                context_node = Node(context_word)
-                context_node.multiplier = multiplier 
-
-            graph.add_edge(target_node, context_node)
-            graph.add_edge(context_node, target_node)
+            if context_word not in to_connect[target_word]:
+                to_connect[target_word].append(context_word)
 
         # Slide window one word over
         buffer.append(words[data_index])
         data_index = (data_index + 1) % len(words)
+    return to_connect 
 
 
 def _score_node(graph, node, iterations=SCORE_ITERATIONS):
